540bcda2afba453f657ecca2f35f94bd5ba4d77b
fpechon <florian.pechon@gmail.com>
2023-04-18 18:37:27 +0200

    Add GLMs & GAMs

--- convert_to_rmd.R
+++ convert_to_rmd.R
@@ -5,7 +5,10 @@
 list_of_files = c("1. Introduction/1. Brief Introduction to R and Descriptive Analysis of the Dataset.ipynb",
                   "2. CART/2. Tree-based models - CART.ipynb",
 				  "3. RandomForest/Tree-based models - RandomForest.ipynb",
-				  "4. GBM/Tree-based models - GBM.ipynb")
+				  "4. GBM/Tree-based models - GBM.ipynb",
+				  "5. GLM/GLM.ipynb",
+				  "5. GLM/Elastic Net.ipynb",
+				  "6. GAM/GAMs.ipynb")
 
 for (file in list_of_files){
   rmarkdown::convert_ipynb(input = file)
